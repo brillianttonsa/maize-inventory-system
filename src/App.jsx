e32538022f1d73ed0528d787e35471b8f3651db9
Abdullatif Mnyamisi <abdullatifmnyamis@gmail.com>
2025-06-04 05:49:52 -0700

    App.jsx set updated v0

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,34 @@
+import { BrowserRouter as Router, Route, Routes,Link } from "react-router-dom"
+import Navbar from "./components/Navbar"
+import React, { useState } from "react"
+import { motion, AnimatePresence } from "framer-motion"
+
+// pages
+import Home from "./pages/Home"
+import About from "./pages/About"
+import Contact from "./pages/Contact"
+import Signup from "./pages/Signup"
 
 
-function App() {
+export default function App() {
+  const [showBox, setShowBox] = useState(false)
 
   return (
-    <>
-    </>
+    <div>
+      <Router>
+        <Navbar/>
+        <Routes>
+          <Route path="/" element={<Home/>}/>
+          <Route path="/about" element={<About/>}/>
+          <Route path="/contact" element={<Contact/>}/>
+          <Route path="/signup" element={<Signup/>}/>
+        </Routes>  
+      </Router>
+     
+
+
+
+      
+    </div>
   )
 }
-
-export default App
