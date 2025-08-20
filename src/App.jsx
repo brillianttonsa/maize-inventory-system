26bbdbaa823da1a4f44b8a46f929e1a3f78a32fd
Abdullatif Mnyamisi <abdullatifmnyamis@gmail.com>
2025-08-20 04:34:52 -0700

    All routes set under Authprovider

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,4 @@
 import { BrowserRouter as Router, Route, Routes,Link } from "react-router-dom"
-import React, { useState } from "react"
-import { motion, AnimatePresence } from "framer-motion"
 
 // pages
 import Home from "./pages/Home"
@@ -10,33 +8,39 @@ import Signup from "./pages/Signup"
 import Login from "./pages/Login"
 import NotFound from "./pages/NotFound"
 import ResetPassword from "./pages/ResetPassword"
+import TermsPage from "./pages/legal/TermsPage"
+import PrivacyPolicyPage from "./pages/legal/PrivacypolicyPage"
 
 //dashboards pages
 // import Dashboard from "./pages/Dashboard"
 
+// protecting routes
+import { AuthProvider } from "./context/AuthContext"
 
 export default function App() {
-  const [showBox, setShowBox] = useState(false)
 
   return (
-    <div>
-      <Router>
-        <Routes>
-          <Route path="/" element={<Home/>}/>
-          <Route path="/about" element={<About/>}/>
-          <Route path="/contact" element={<Contact/>}/>
-          <Route path="/signup" element={<Signup/>}/>
-          <Route path="/login" element={<Login/>}/>
-          <Route path="/reset-password" element={<ResetPassword/>}/>
-          {/* <Route path="/dashboard" element={
-            <ProtectedRoute>
-              <Dashboard/>
-            </ProtectedRoute>
-          } /> */}
-          {/* <Route path="/dashboard" element={<Dashboard/>} /> */}
-          <Route path="*" element={<NotFound />} />
-        </Routes>  
-      </Router>
-    </div>
+      <AuthProvider>
+        <Router>
+          <Routes>
+            <Route path="/" element={<Home/>}/>
+            <Route path="/about" element={<About/>}/>
+            <Route path="/contact" element={<Contact/>}/>
+            <Route path="/signup" element={<Signup/>}/>
+            <Route path="/login" element={<Login/>}/>
+            <Route path="/reset-password" element={<ResetPassword/>}/>
+            <Route path="/terms-of-service" element={<TermsPage/>}/>
+            <Route path="private-policy" element={<PrivacyPolicyPage/>}/>
+
+            {/* <Route path="/dashboard" element={
+              <ProtectedRoute>
+                <Dashboard/>
+              </ProtectedRoute>
+            } /> */}
+            {/* <Route path="/dashboard" element={<Dashboard/>} /> */}
+            <Route path="*" element={<NotFound />} />
+          </Routes>  
+        </Router>
+      </AuthProvider>
   )
 }
